30f22d5e3c7b63070867e380f7cfad75b75bf92b
matheo-1712 <perod.matheo@gmail.com>
2026-06-18 15:56:26 +0200

    Rename Dark Immune ability to Gloomight and update references

--- data/cobblemon/abilities/gloomight.js
+++ data/cobblemon/abilities/gloomight.js
@@ -1,12 +1,12 @@
 ({
     onTryHit(target, source, move) {
     if (target !== source && move.type === "Dark") {
-            this.add("-immune", target, "[from] ability: Dark immune");
+            this.add("-immune", target, "[from] ability: Gloomight");
         return null;
     }
 },
     flags: { breakable: 1 },
-    name: "Dark immune",
+    name: "Gloomight",
         rating: 3,
     num: 1000
 })
